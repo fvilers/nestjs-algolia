a006cc60a4121a9aecd8f6bdfe64565758a3d707
Fabian Vilers <fabian.vilers@dev-one.com>
2019-02-25 11:11:26 +0100

    Add an async register to use dependency when registering

--- lib/algolia-module-options.ts
+++ lib/algolia-module-options.ts
@@ -1,7 +1,22 @@
+import { ModuleMetadata, Type } from '@nestjs/common/interfaces';
 import { ClientOptions } from 'algoliasearch';
 
 export interface AlgoliaModuleOptions {
   applicationId: string;
   apiKey: string;
   clientOptions?: ClientOptions;
+}
+
+export interface AlgoliaOptionsFactory {
+  createAlgoliaOptions(): Promise<AlgoliaModuleOptions> | AlgoliaModuleOptions;
+}
+
+export interface AlgoliaModuleAsyncOptions
+  extends Pick<ModuleMetadata, 'imports'> {
+  useExisting?: Type<AlgoliaOptionsFactory>;
+  useClass?: Type<AlgoliaOptionsFactory>;
+  useFactory?: (
+    ...args: any[]
+  ) => Promise<AlgoliaModuleOptions> | AlgoliaModuleOptions;
+  inject?: any[];
 }
--- lib/algolia.module.ts
+++ lib/algolia.module.ts
@@ -1,6 +1,10 @@
-import { Module, DynamicModule } from '@nestjs/common';
+import { Module, DynamicModule, Provider } from '@nestjs/common';
 import { AlgoliaService } from './algolia.service';
-import { AlgoliaModuleOptions } from './algolia-module-options';
+import {
+  AlgoliaModuleOptions,
+  AlgoliaModuleAsyncOptions,
+  AlgoliaOptionsFactory,
+} from './algolia-module-options';
 import { createAlgoliaClient } from './algolia-client.provider';
 import { ALGOLIA_MODULE_OPTIONS } from './algolia.constants';
 
@@ -16,6 +20,49 @@ export class AlgoliaModule {
         createAlgoliaClient(),
         { provide: ALGOLIA_MODULE_OPTIONS, useValue: options },
       ],
+    };
+  }
+
+  static registerAsync(options: AlgoliaModuleAsyncOptions): DynamicModule {
+    return {
+      module: AlgoliaModule,
+      imports: options.imports || [],
+      providers: [createAlgoliaClient(), ...this.createAsyncProviders(options)],
+    };
+  }
+
+  private static createAsyncProviders(
+    options: AlgoliaModuleAsyncOptions,
+  ): Provider[] {
+    if (options.useExisting || options.useFactory) {
+      return [this.createAsyncOptionsProvider(options)];
+    }
+
+    return [
+      this.createAsyncOptionsProvider(options),
+      {
+        provide: options.useClass,
+        useClass: options.useClass,
+      },
+    ];
+  }
+
+  private static createAsyncOptionsProvider(
+    options: AlgoliaModuleAsyncOptions,
+  ): Provider {
+    if (options.useFactory) {
+      return {
+        provide: ALGOLIA_MODULE_OPTIONS,
+        useFactory: options.useFactory,
+        inject: options.inject || [],
+      };
+    }
+
+    return {
+      provide: ALGOLIA_MODULE_OPTIONS,
+      useFactory: async (optionsFactory: AlgoliaOptionsFactory) =>
+        await optionsFactory.createAlgoliaOptions(),
+      inject: [options.useExisting || options.useClass],
     };
   }
 }
