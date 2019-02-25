61cd94f6806408f9c8ab0c32a6e5791855da8411
Fabian Vilers <fabian.vilers@dev-one.com>
2019-02-25 09:29:11 +0100

    Add the Agolia service

--- lib/algolia.module.ts
+++ lib/algolia.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { AlgoliaService } from './algolia.service';
 
-@Module({})
+@Module({
+  providers: [AlgoliaService],
+  exports: [AlgoliaService],
+})
 export class AlgoliaModule {}
